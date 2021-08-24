3895f9ef821a93f98708e986957b5f6012fca1b3
andy <deifmo@yandex.ru>
2021-08-24 17:21:20 +0300

    DSF comments added, code refactored

--- depth_search_first.py
+++ depth_search_first.py
@@ -1,35 +1,30 @@
 from stack_data_structure import Stack
 
 
-def depth_first_search_iterative(graph, start):
-    s, visited = Stack([start]), [start]
+def depth_first_search(adj_list, start):
+    """
+    Iterative version implemented via stack.
+    :param adj_list: graph as adjacency list.
+    :param start: starting vertex.
+    :return: list of visited (processed) vertices.
+    """
+    # unvisited - stack with vertices waiting for processing.
+    # visited - array with vertices already being processed.
+    unvisited, visited = Stack([start]), []
 
-    while s:
+    while unvisited:
+        vertex = unvisited.pop()
+        # if vertex is not processed yet - process it.
+        if vertex not in visited:
+            # process(vertex) with using appropriate function here.
+            # then adds it to the 'visited' list.
+            visited.append(vertex)
 
-        vertex = s.pop()
+            for neighbor in adj_list[vertex]:
+                # if neighbor is not processed yet, adds it to the stack.
+                if neighbor not in visited:
+                    unvisited.push(neighbor)
 
-        for neighbor in graph[vertex]:
-            if neighbor not in visited:
-                s.push(neighbor)
-                visited.append(neighbor)
-
+    # main purpose of DSF is to process every vertex instead of returning 'visited' list,
+    # but it's returned to ensure every vertex in connected graph is visited.
     return visited
-
-
-def depth_first_search_recursive(graph, vertex, visited=None):
-    visited = [] if visited is None else visited
-    visited.append(vertex)
-
-    for neighbor in graph[vertex]:
-        if neighbor not in visited:
-            depth_first_search_recursive(graph, neighbor, visited)
-
-    return visited
-
-
-graph1 = [
-    [0, 6, 2, 0],
-    [6, 0, 3, 1],
-    [2, 3, 0, 5],
-    [0, 1, 5, 0]
-]
