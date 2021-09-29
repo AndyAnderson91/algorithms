0bf6c2881ff6faf0c3a4ecdb3585b772358e4158
andy <deifmo@yandex.ru>
2021-09-29 22:52:05 +0300

    Used unmutable sequences as argument, separate tests for in-place and out-of-place algorithms

--- tests/test_sorting_algorithms.py
+++ tests/test_sorting_algorithms.py
@@ -1,35 +1,56 @@
 """
 Tests for sorting algorithms.
-All functions have identical input and output,
-so same tests are used for all functions.
 """
 import pytest
 from sorting_algorithms import bubble_sort, selection_sort, insertion_sort, merge_sort, quick_sort
 
 
+# Constants
+
+SEQUENCES = [
+    # Using tuples to keep base sequences unsorted.
+    (),
+    (3, ),
+    (1, 2),
+    (0, 9, 0),
+    (3, 0, 5, 5),
+    (1, 4, 3, 2, 0),
+    (7, -5, 3, 1, 8, 3),
+    ('ah', 'ak', 'abc', 'a'),
+    (9, -4, -3, 0, 12, 2, 7, 5, -3),
+    (5, 2, 1, 0, 12, 4, -3, -2, 18, -14, 1),
+]
+
+
+# Fixtures
+
 @pytest.fixture(params=[
     bubble_sort,
     selection_sort,
     insertion_sort,
-    merge_sort,
-    quick_sort
 ])
-def sorting_function(request):
+def in_place_sorting_function(request):
     return request.param
 
 
-@pytest.mark.parametrize('array', [
-    [],
-    [3],
-    [1, 2],
-    [0, 9, 0],
-    [3, 0, 5, 5],
-    [1, 4, 3, 2, 0],
-    [7, -5, 3, 1, 8, 3],
-    ['ah', 'ak', 'abc', 'a'],
-    [9, -4, -3, 0, 12, 2, 7, 5, -3],
-    [5, 2, 1, 0, 12, 4, -3, -2, 18, -14, 1]
+@pytest.fixture(params=[
+    merge_sort,
+    quick_sort,
 ])
-def test_sorting_function(sorting_function, array):
-    sorting_function(array)
+def out_of_place_sorting_function(request):
+    return request.param
+
+
+# Tests
+
+@pytest.mark.parametrize('sequence', SEQUENCES)
+def test_in_place_sorting_functions(in_place_sorting_function, sequence):
+    array = list(sequence)
+    in_place_sorting_function(array)
     assert array == sorted(array)
+
+
+@pytest.mark.parametrize('sequence', SEQUENCES)
+def test_out_of_place_sorting_functions(out_of_place_sorting_function, sequence):
+    array = list(sequence)
+    assert out_of_place_sorting_function(array) == sorted(array)
